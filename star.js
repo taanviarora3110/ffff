2f5e5ca3b3dcf4b1db152ac1ab3f3d854c046f68
taanviarora3110 <78641755+taanviarora3110@users.noreply.github.com>
2021-06-08 17:38:56 +0530

    Add files via upload

---
+++ star.js
@@ -0,0 +1,37 @@
+class Star
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+			restitution:1,
+			friction:0,
+			density:0.8
+			
+			}
+		this.x=x;
+		this.y=y;
+		this.r=r
+		this.image = loadImage("images/star.png");
+		this.body=Bodies.circle(this.x, this.y, (this.r)/2, options)
+		World.add(world, this.body);
+		
+		
+	}
+	display()
+	{
+			
+			var starPos=this.body.position;
+			push();
+			translate(starPos.x, starPos.y);
+			rectMode(CENTER)
+			//strokeWeight(4);
+			fill(255,0,255)
+			//imageMode(CENTER);
+			image(this.image,0,0,this.r, this.r)
+			ellipse(0,0,this.r, this.r);
+			pop();
+			
+	}
+
+}
